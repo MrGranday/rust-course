fefe71a9d2f9d62bdcb83a22e609482011dba423
Osman Ghani Granday <osmamghani009009@gmail.com>
2025-09-06 00:59:42 -0700

    day8 with memory

--- src/day8.rs
+++ src/day8.rs
@@ -1,12 +1,12 @@
 //Day8
 //
-// Basic memory refresh
+// *Basic memory refresh*/
 //memory is stored using binary // Bits: 0 or 1
 //Computer optimized for bytes // 1 byte == 8 contiguous bits
 //fully contiguous
 
 //
-//Addresses
+//*Addresses*/
 // all data in the memory has an "address"
 //used to locate data
 //always the same - only data changes
@@ -15,7 +15,7 @@
 
 //
 //
-//Offset
+//*Offset8*/
 //Items can be located at an address using an "offset"
 //Offsets begin at 0
 // Represent the number of bytes away from the original address
@@ -24,3 +24,32 @@
 //memory uses addresses and offsets
 //addresses are permanent, data differs
 //offsets can be used to "index" into some data
+
+//*Ownership */
+//
+//Programs must track memory
+//if they fail to do so a leak occurs
+//Rust utilizes an "ownership" model to manage memory
+//the "owner"of memory is responsible for cleaning up the memory
+// memory can either be "moved" or "borrowed"
+//*Example of - Move */
+//
+//
+
+// *enum Light {
+//*      Bright,
+// *    Dull,
+// *}
+
+//* fn display_light(light: Light) {
+// *    match light {
+// *       Light::Bright => println!("bright"),
+// *       Light::Dull => println!("dull"),
+// *      }
+// *}
+
+// *fn main() {
+// *    let dull = Light::Dull;
+// *    display_light(dull);
+// *    display_light(dull);
+// * }
