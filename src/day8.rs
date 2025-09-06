82cad2549c5500ac3f2a9ad37020ff6c16959319
Osman Ghani Granday <osmamghani009009@gmail.com>
2025-09-06 00:23:22 -0700

    day8 with memory

---
+++ src/day8.rs
@@ -0,0 +1,26 @@
+//Day8
+//
+// Basic memory refresh
+//memory is stored using binary // Bits: 0 or 1
+//Computer optimized for bytes // 1 byte == 8 contiguous bits
+//fully contiguous
+
+//
+//Addresses
+// all data in the memory has an "address"
+//used to locate data
+//always the same - only data changes
+//usually don't utilize addresses directly
+// variable handle most of the work
+
+//
+//
+//Offset
+//Items can be located at an address using an "offset"
+//Offsets begin at 0
+// Represent the number of bytes away from the original address
+//normally deal with indexes instead
+
+//memory uses addresses and offsets
+//addresses are permanent, data differs
+//offsets can be used to "index" into some data
